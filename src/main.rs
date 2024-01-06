08be9e654dd4268b83dcb9e39c66ab384e48335f
Jonathan Woollett-Light <jonathanwoollettlight@gmail.com>
2024-01-06 11:07:47 +0000

    example commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world!");
     println!("I'm going for a walk");
-    println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    println!("aaaaaaaaaaaaaaaaaaaa");
     println!("Goodbye, world!");
 }
